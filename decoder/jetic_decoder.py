a7c3db46fa2220e8f4206b26268d7d9396e397a1
Jetic Gu <jetic@me.com>
2016-11-05 17:44:03 -0700

    Naive decoder(exponential)

--- decoder/jetic_decoder.py
+++ decoder/jetic_decoder.py
@@ -4,35 +4,38 @@ import sys
 import models
 from math import log
 from collections import namedtuple
+from copy import deepcopy
 
 ngram_stats = namedtuple("ngram_stats", "logprob, backoff")
+phrase = namedtuple("phrase", "english, logprob")
 
 
 class TargetSentence():
     def __init__(self,
-                 lm,
                  length=0,
                  sourceMark=[],
                  targetSentenceEntity=(),
                  tmScore=0.0,
-                 lmScore=0.0):
-
+                 key=None):
+        if key:
+            self.sourceMark, self.targetSentenceEntity = key
+            self.sourceMark = list(self.sourceMark)
+            self.tmScore = tmScore
+            return
         if length == 0 and sourceMark == []:
             raise ValueError("SENTENCE [ERROR]: Invalid initialisation")
         if length != 0 and sourceMark == []:
             self.sourceMark = [0 for x in range(length)]
+            self.targetSentenceEntity = targetSentenceEntity
         else:
             self.sourceMark = sourceMark
-        self.targetSentenceEntity = targetSentenceEntity
+            self.targetSentenceEntity = targetSentenceEntity
         self.tmScore = tmScore
-        self.lmScore = lmScore
-        self.lm = lm
-        self.lm_state = lm.begin()
         return
 
     def key(self):
         # Generate the unique key for the sentence
-        key = {sourceMark: self.sourceMark, entity: self.targetSentenceEntity}
+        key = (tuple(self.sourceMark), self.targetSentenceEntity)
         return key
 
     def overlapWithPhrase(self, phraseStartPosition, phraseEndPosition):
@@ -45,21 +48,29 @@ class TargetSentence():
         # mark positions in sourceMark as translated
         for i in range(phraseStartPosition, phraseEndPosition):
             self.sourceMark[i] = 1
-
         # add target phrase to sentence
-        self.targetSentenceEntity = self.targetSentenceEntity + targetPhrase.english.split()
-
-        # calculate language score
-        lm_state = lm.begin()
-        self.lmScore = 0.0
-        for word in self.targetSentenceEntity:
-            (lm_state, word_logprob) = lm.score(lm_state, word)
-            logprob += word_logprob
-        self.lmScore += lm.end(lm_state)
-
+        self.targetSentenceEntity = self.targetSentenceEntity + tuple(targetPhrase.english.split())
         # update translation score
         self.tmScore += targetPhrase.logprob
         return
+
+    def lmScore(self, lm):
+        # calculate language score
+        lm_state = lm.begin()
+        lmScore = 0.0
+        for word in self.targetSentenceEntity:
+            (lm_state, word_logprob) = lm.score(lm_state, word)
+            logprob += word_logprob
+        lmScore += lm.end(lm_state)
+        return lmScore
+
+    def totalScore(self, lm):
+        return self.lmScore(lm) + self.tmScore
+
+    def completed(self):
+        if sum(self.sourceMark) == len(self.sourceMark):
+            return True
+        return False
 
 
 class Decoder():
@@ -110,24 +121,66 @@ class Decoder():
         return
 
     def decode(self, sentence, max_phrase=5):
-        stack = [()]
-        newStack = []
-        for i in range(sentence):
+        bestScore = - sys.maxint - 1
+        bestSentence = ()
+        emptyTargetSentence = TargetSentence(length=len(sentence))
+        stack = {}
+        stack[emptyTargetSentence.key()] = emptyTargetSentence.tmScore
+        newStack = {}
+        for i in range(len(sentence)):
+            sys.stderr.write("processing the " + str(i) + "th phrase, stack size: " + str(len(stack)) + "\n")
             # adding the ith target word/phrase
-            for j in range(sentence):
+            for j in range(len(sentence)):
                 # choose the jth source word as a start
-                for k in range(j+1, max(len(sentence), j+max_phrase+1)):
+                for k in range(j+1, min(len(sentence), j+max_phrase+1)):
                     # the phrase choosen to add this time is from j to k
                     sourcePhrase = sentence[j:k]
+                    # Skip if the phrase doesn't exist
+                    if sourcePhrase not in self.tm:
+                        continue
+
                     for targetPhrase in self.tm[sourcePhrase]:
                         # for each translation, combine with every existing targetSentence in stack
-                        for targetSentence in stack:
-                            # Check if the sourcePhrase overlaps with the one in targetSentence
-                            # Combine targetSentence and targetPhrase
-                            # add the combined targetSentence to newStack
-                            raise NotImplemented
+                        for targetSentenceKey in stack:
+                            # Reconstruct sentence
+                            targetSentence = TargetSentence(key=targetSentenceKey,
+                                                            tmScore=stack[targetSentenceKey])
+
+                            # Check if overlapped
+                            if targetSentence.overlapWithPhrase(j, k):
+                                # overlapped, skip this sentence
+                                continue
+
+                            # Add phrase to sentence
+                            targetSentence.addPhrase(j, k, targetPhrase)
+
+                            # Compare with best score if translation complete, and skip adding to newStack
+                            if targetSentence.completed():
+                                if targetSentence.score() > bestScore:
+                                    bestScore = targetSentence.score()
+                                    bestSentence = targetSentence.targetSentenceEntity
+                            else:
+                                # Add the combined targetSentence to newStack if translation incomplete
+                                key = targetSentence.key()
+                                if key in newStack:
+                                    if targetSentence.tmScore > newStack[key]:
+                                        newStack[key] = targetSentence.tmScore
+                                else:
+                                    newStack[key] = targetSentence.tmScore
+                            # Current targetSentences processed, proceed with next targetSentence in stack
+
+                        # All targetSentences in stack processed, proceed with next translation
+
+                    # All translations processed, proceed with next phrase
+
+                # All phrases processed, proceed with next starting position for phrase
+
+            # The ith phrase added to newStack. Exchange newStack and stack
             stack = newStack
-            newStack = []
+            newStack = {}
+
+        # All words processed, we now have the best sentence stored in bestSentence
+        print bestSentence
         return
 
 
