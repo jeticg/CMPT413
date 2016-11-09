1caf45f445518ab12ead5e659c314cb3ed3fad41
Jetic Gu <jetic@me.com>
2016-11-09 10:55:15 -0800

    Removing default decoder

--- decoder/jetic_decoder.py
+++ decoder/jetic_decoder.py
@@ -17,43 +17,6 @@ class Decoder():
         self.answers = []
         return
 
-    def decodeDefault(self,
-               sentence):
-
-        f = sentence
-        tm = self.tm
-        hypothesis = namedtuple("hypothesis", "logprob, lm_state, predecessor, phrase")
-        initial_hypothesis = hypothesis(0.0, self.lm.begin(), None, None)
-        stacks = [{} for _ in f] + [{}]
-        stacks[0][self.lm.begin()] = initial_hypothesis
-        for i, stack in enumerate(stacks[:-1]):
-            for h in sorted(stack.itervalues(), key=lambda h: -h.logprob)[:opts.s]:  # prune
-                for j in xrange(i + 1, len(f) + 1):
-                    if f[i:j] in self.tm:
-                        for phrase in self.tm[f[i:j]]:
-                            logprob = h.logprob + phrase.logprob
-                            lm_state = h.lm_state
-                            for word in phrase.english.split():
-                                (lm_state, word_logprob) = self.lm.score(lm_state, word)
-                                logprob += word_logprob
-                            logprob += self.lm.end(lm_state) if j == len(f) else 0.0
-                            new_hypothesis = hypothesis(logprob, lm_state, h, phrase)
-                            if lm_state not in stacks[j] or stacks[j][lm_state].logprob < logprob:  # second case is recombination
-                                stacks[j][lm_state] = new_hypothesis
-        winner = max(stacks[-1].itervalues(), key=lambda h: h.logprob)
-
-        def extract_english(h):
-            return "" if h.predecessor is None else "%s%s " % (extract_english(h.predecessor), h.phrase.english)
-        print extract_english(winner)
-
-        if opts.verbose:
-            def extract_tm_logprob(h):
-                return 0.0 if h.predecessor is None else h.phrase.logprob + extract_tm_logprob(h.predecessor)
-            tm_logprob = extract_tm_logprob(winner)
-            sys.stderr.write("LM = %f, TM = %f, Total = %f\n" %
-                (winner.logprob - tm_logprob, tm_logprob, winner.logprob))
-        return
-
     def decode(self,
                sentence,
                maxPhraseLen=10,
